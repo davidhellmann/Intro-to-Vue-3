a3e8d3ed2ba14d3d9668c52b1442cfaa9848dd93
davidhellmann <david@hellmann.io>
2020-09-17 00:45:52 +0200

    Update

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      productDesc: 'The quick brown fox jumps over the lazy dogs'
+    }
+  },
+})
